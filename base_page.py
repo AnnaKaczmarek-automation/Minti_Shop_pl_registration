16d2c12ee6ee15248496acd4ec64fa444c1b9580
Annifrid <56770220+Annifrid@users.noreply.github.com>
2020-11-13 08:20:40 +0100

    Delete base_page.py

--- base_page.py
+++
@@ -1,8 +0,0 @@
-class BasePage:
-    def __init__(self, driver):
-        self.driver = driver
-        self._verify_page()
-        print("sprawdzam")
-
-    def _verify_page(self):
-        return
